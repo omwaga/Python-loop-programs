f21e0eb3097fc5c1c7939073c00436b14ce49143
Collins Omwaga <collinsomwaga@gmail.com>
2019-07-27 10:31:43 +0300

    Odd or even Update

---
+++ Check_Leap_Year.py
@@ -0,0 +1,18 @@
+"""
+Leap Year:
+
+A year is called a leap year if it contains an additional day which makes the number of the days in that year to be 366.
+This additional day is added in February which makes it 29 days long.
+
+A leap year occurs once every 4 years.
+
+How to determine if a year is a leap year?
+
+You should follow the following steps to determine whether a year is a leap year or not.
+
+If a year is evenly divisible by 4 means having no remainder then go to next step. If it is not divisible by 4.
+It is not a leap year. For example: 1997 is not a leap year.
+If a year is divisible by 4, but not by 100. For example: 2012, it is a leap year. If a year is divisible by both 4 and 100, go to next step.
+If a year is divisible by 100, but not by 400. For example: 1900, then it is not a leap year. If a year is divisible by both,
+then it is a leap year. So 2000 is a leap year.
+"""
--- Odd_or_Even.py
+++ Odd_or_Even.py
@@ -1,5 +1,5 @@
 num = int(input("Enter a number"))
-if(num%2==0):
+if(num % 2) == 0:
     print("{0} is Even Number".format(num))
 else:
     print(("{0} is Odd Number".format(num)))
