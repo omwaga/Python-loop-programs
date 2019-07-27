3315f51cb9afac1dfcda99fb0aa213cd35230f0b
Collins Omwaga <collinsomwaga@gmail.com>
2019-07-27 10:46:43 +0300

    Check Laep year

--- Check_Leap_Year.py
+++ Check_Leap_Year.py
@@ -16,3 +16,14 @@ If a year is divisible by 4, but not by 100. For example: 2012, it is a leap yea
 If a year is divisible by 100, but not by 400. For example: 1900, then it is not a leap year. If a year is divisible by both,
 then it is a leap year. So 2000 is a leap year.
 """
+Year = int(input("Enter a year:"))
+if(Year % 4) == 0:
+    if(Year % 100) == 0:
+        if(Year % 400) == 0:
+            print("{0} is a Leap year".format(Year))
+        else:
+            print("{0} is not a Leap Year".format(Year))
+    else:
+        print("{0} is a Leap Year".format(Year))
+else:
+    print("{0} is not a Leap Year.".format(Year))
