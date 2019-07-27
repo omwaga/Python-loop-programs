c9a4648e973898cdb474433b1484bea80812ab76
Collins Omwaga <collinsomwaga@gmail.com>
2019-07-27 09:56:18 +0300

    Positive, Negative and Zero check

---
+++ Positive_Negative _Zero_Check.py
@@ -0,0 +1,7 @@
+num = float(input("Enter a number"))
+if num > 0:
+    print("{0} is a postive number".format(num))
+elif num == 0:
+    print("{0} is zero".format(num))
+else:
+    print("{0} is a negative number".format(num))
