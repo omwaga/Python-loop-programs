4300f422d5baf0fd1a92fb3e33c88bb9deda0061
Collins Omwaga <collinsomwaga@gmail.com>
2019-07-28 12:09:40 +0300

    Factorial of a number

--- Factorial_of_a_Number.py
+++ Factorial_of_a_Number.py
@@ -3,3 +3,15 @@ Factorial is a non-negative integer.
 It is the product of all positive integers less than or equal to that number for which you ask for factorial.
  It is denoted by exclamation sign (!).
  """
+
+num = int(input("Enter a number:"))
+
+factorial = 1
+if num < 0:
+    print("Sorry, factorial does not exist for negative numbers")
+elif num == 0:
+    print("The factorial of 0 is 1")
+else:
+    for i in range(1, num + 1):
+        factorial = factorial * i
+    print("The factorial of", num, "is", factorial)
