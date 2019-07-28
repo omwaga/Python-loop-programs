b2b01ce2f085bad6ced45af5a0f4a31ba65d49bf
Collins Omwaga <collinsomwaga@gmail.com>
2019-07-28 10:50:16 +0300

    print prime numbers in a range

---
+++ Factorial_of_a_Number.py
@@ -0,0 +1,5 @@
+"""
+Factorial is a non-negative integer.
+It is the product of all positive integers less than or equal to that number for which you ask for factorial.
+ It is denoted by exclamation sign (!).
+ """
--- Positive_Negative _Zero_Check.py
+++ Positive_Negative _Zero_Check.py
@@ -5,3 +5,8 @@ elif num == 0:
     print("{0} is zero".format(num))
 else:
     print("{0} is a negative number".format(num))
+
+"""
+In the above example, elif statement is used. 
+The elif statement is used to check multiple expressions for TRUE and executes a block of code when one of the conditions becomes TRUE.
+"""
